05b9f9d7736dc9c4d8c26e2d1cc562c18a0ee650
Thanh Pham <thanhptr@gmail.com>
2018-03-19 23:56:08 +0700

    Export config types

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -1,3 +1,4 @@
+export * from "./config";
 export * from "./register";
 export * from "./lib";
 export * from "./auth/express";
--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./config"));
 __export(require("./register"));
 __export(require("./lib"));
 __export(require("./auth/express"));
